f5cbd0fdd1e90d805f62ce4b96284a5295ef59d6
zhujinwei <331371812@qq.com>
2017-04-27 18:05:16 +0800

    获取到图片了，下一步更改RecyclerView和本地缓存

---
+++ app/src/main/java/com/navgnss/gankio/adapter/FluidAdapter.java
@@ -0,0 +1,82 @@
+package com.navgnss.gankio.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.navgnss.gankio.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by ZhuJinWei on 2017/4/27.
+ */
+
+public class FluidAdapter extends RecyclerView.Adapter<FluidAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<String> datas;
+
+    public FluidAdapter(Context context,List<String> datas){
+
+        this.context= context;
+        this.datas= datas;
+
+    }
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        MyViewHolder viewHolder=new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_fluid,parent,false));
+        return viewHolder;
+    }
+
+    /**
+     * binding时实现瀑布流效果
+     * */
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+
+
+        Picasso.with(context).load(datas.get(position*2)).into(holder.iv);
+        holder.tv.setText(datas.get(position*2+1));
+
+        holder.iv.setLayoutParams(new LinearLayout.LayoutParams(80,60+20*(int)Math.random()));
+
+    }
+
+    public void addAll(List<String> mDatas,boolean isClear){
+        if(isClear)
+            datas.clear();
+
+        datas.addAll(mDatas);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView iv;
+        TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            iv=(ImageView) itemView.findViewById(R.id.item_meizi_iv);
+            tv=(TextView) itemView.findViewById(R.id.item_meizi_tv);
+        }
+    }
+}
